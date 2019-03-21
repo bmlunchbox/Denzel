b05af54fa081b1e86f204f70cc7b39d548ba9cce
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-21 01:39:05 -0400

    get course data and table data structure

---
+++ data/courses.py
@@ -0,0 +1,28 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+# will have to change the 1920 to year if that's a feature
+BASE = "http://www.ucalendar.uwaterloo.ca/1920/COURSE/"
+department_urls = ['course-MSCI.html']
+
+
+def get_courses():
+    page_url = BASE + department_urls[0]
+
+    html = urllib.request.urlopen(page_url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    courses = soup.find_all('center')
+    for course in courses:
+        # index 0 is code, index 1 is name
+        bolded = course.find_all('b')
+
+        description = course.find_all('td')[3].text
+
+        italicized = course.find_all('i')
+        for i in italicized:
+            if i.text.strip():
+                print(i.text)
+
+
+
--- data/requirements.py
+++ data/requirements.py
@@ -11,13 +11,22 @@ def get_requirements():
     html = urllib.request.urlopen(page_url).read()
     soup = BeautifulSoup(html, 'html.parser')
 
+    term = ""
+    electives = 0
+
     # this only works for management for now
     table = soup.find_all('table')[5]
     for rows in table.find_all('tr')[1:]:
         header = rows.find_all('th')
         if header:
-            print(header[0].text)
+            term = header[0].text
+            print(term)
 
         cell = rows.find_all('td')
         if cell:
-            print(cell[0].text.strip())
+            course = cell[0].text.strip()
+            if course == "Elective":
+                electives += 1
+            print(course)
+
+    print(electives)
