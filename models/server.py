fcaa87c99303506da33c4d7dc1051ee8147f3a2f
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-25 13:15:51 -0400

    initial commit for mongo connections and schema

---
+++ models/server.py
@@ -0,0 +1,7 @@
+import dns
+from pymongo import MongoClient
+
+
+def connect():
+    client = MongoClient("mongodb+srv://Brandon:<pw>@coursedata-u7jmw.mongodb.net/test?retryWrites=true")
+    db = client.Courses
