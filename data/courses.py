7364640030dd6c19dd35552424e9ac1e5c9836f2
Brandon Leung <brandonmichael.kg@gmail.com>
2019-03-25 12:38:56 -0400

    scraping progress

--- data/courses.py
+++ data/courses.py
@@ -6,7 +6,7 @@ BASE = "http://www.ucalendar.uwaterloo.ca/1920/COURSE/"
 department_urls = ['course-MSCI.html']
 
 
-def get_courses():
+def department_courses():
     page_url = BASE + department_urls[0]
 
     html = urllib.request.urlopen(page_url).read()
@@ -24,5 +24,7 @@ def get_courses():
             if i.text.strip():
                 print(i.text)
 
+# def ratings(course):
 
-
+    # to be called with above information
+    # dependency to get JavaScript rendered information
--- index.py
+++ index.py
@@ -1,3 +1,13 @@
-from data import requirements
+from data import requirements, courses
+from models import server
 
-requirements.get_requirements()
+# Update Database button in UI
+# run scripts in data file
+# connect to mongo and write to it
+
+# request for computation; receive parameters from frontend
+# get information for computations (read from DB)
+# compute courses and rejected available ones
+# return as package in API
+
+server.connect()
